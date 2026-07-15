8b9e72ceb2d2c3ae12945d3b25a73ad40d4c511d
Nilson <netohjunior216@gmail.com>
2026-07-15 00:09:14 -0300

    feat: criado LoginPage

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,12 +1,10 @@
 
-
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
+import LoginForm from "./components/login-form"
 import SignUpForm  from "./components/sign-up-form"
 
 const AutheticationPage = () => {
-
   return (
     <div className="flex h-screen w-screen items-center justify-center">
        <Tabs defaultValue="login" className="w-[400px]">
@@ -15,6 +13,7 @@ const AutheticationPage = () => {
           <TabsTrigger value="register">Criar conta</TabsTrigger>
         </TabsList>
         <TabsContent value="login">
+          <LoginForm />
         </TabsContent>
         <TabsContent value="register">
           <SignUpForm />
