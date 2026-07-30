0dd909f48cf39382beb1cb6c02594a6711cdde0f
Nilson <netohjunior216@gmail.com>
2026-07-30 19:54:55 -0300

    feat: implement SubscriptionPlan component for managing user subscriptions and add Stripe webhook for handling subscription events; create new subscription page with user session checks

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,26 +1,22 @@
-
-import { headers } from "next/headers"
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { auth } from "@/lib/auth"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { auth } from "@/lib/auth";
 
-import LoginForm from "./components/login-form"
-import SignUpForm  from "./components/sign-up-form"
+import LoginForm from "./components/login-form";
+import SignUpForm from "./components/sign-up-form";
 
-
-const AutheticationPage = async () => {
+const AuthenticationPage = async () => {
   const session = await auth.api.getSession({
-  headers: await headers(),
-})
-
-  if(session?.user){
-    redirect("/dashboard")
+    headers: await headers(),
+  });
+  if (session?.user) {
+    redirect("/dashboard");
   }
-
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-       <Tabs defaultValue="login" className="w-[400px]">
+      <Tabs defaultValue="login" className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="login">Login</TabsTrigger>
           <TabsTrigger value="register">Criar conta</TabsTrigger>
@@ -33,12 +29,7 @@ const AutheticationPage = async () => {
         </TabsContent>
       </Tabs>
     </div>
-  )
-}
+  );
+};
 
-export default AutheticationPage
-
-
-// ERROR TypeError: (0 , react_hook_form__WEBPACK_IMPORTED_MODULE_8__.useForm) is not a function
-// INDENTIFICAR USAR APENAS LADO DO CLIETE user client
- 
+export default AuthenticationPage;
