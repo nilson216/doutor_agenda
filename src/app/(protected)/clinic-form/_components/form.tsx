1526c068f74d2bb643f4c7da2d25fb455f3e7471
Nilson <netohjunior216@gmail.com>
2026-07-30 22:00:15 -0300

    fix: update import path for createClinic action to ensure correct module resolution

--- src/app/(protected)/clinic-form/_components/form.tsx
+++ src/app/(protected)/clinic-form/_components/form.tsx
@@ -7,7 +7,7 @@ import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
 
-import { createClinic } from "@/actions/create-clinic";
+import { createClinic } from "@/actions/create-clinic/index";
 import { Button } from "@/components/ui/button";
 import { DialogFooter } from "@/components/ui/dialog";
 import {
