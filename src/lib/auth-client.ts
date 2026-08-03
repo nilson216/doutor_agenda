83f1f294fd045e02284d2046da907ecba36e95bc
Nilson <netohjunior216@gmail.com>
2026-08-02 22:00:30 -0300

    feat: add WithAuthentication HOC for session validation and refactor AppointmentsPage to utilize it; update button component styling and improve table component classes

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
-import { customSessionClient } from "better-auth/client/plugins"
-import { createAuthClient } from "better-auth/react"
+import { customSessionClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
 
-import { auth } from "./auth"
+import { auth } from "./auth";
+
 export const authClient = createAuthClient({
-    plugins: [customSessionClient<typeof auth>()]
-})
+  plugins: [customSessionClient<typeof auth>()],
+});
