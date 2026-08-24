313c399bc798836067a9b15d83a754cb7365c64a
Nilson <netohjunior216@gmail.com>
2026-08-23 22:30:57 -0300

    refactor: enhance middleware to allow public access to authentication paths and streamline session validation logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,22 +2,25 @@ import { getSessionCookie } from "better-auth/cookies";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
+const publicPaths = ["/authentication"];
+
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (publicPaths.some((path) => pathname.startsWith(path))) {
+    return NextResponse.next();
+  }
+
   const sessionCookie = getSessionCookie(request);
   if (!sessionCookie) {
     return NextResponse.redirect(new URL("/authentication", request.url));
   }
+
   return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: [
-    "/dashboard",
-    "/patients",
-    "/doctors",
-    "/appointments",
-    "/subscription",
     "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
   ],
 };
